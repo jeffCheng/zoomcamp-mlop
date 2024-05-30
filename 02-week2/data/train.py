5e78478392dfc258d2337ee519ab358b1cd9859a
jeffCheng <jeff32191182@gmail.com>
2024-05-30 08:10:57 +0000

    eee243

--- 02-week2/data/train.py
+++ 02-week2/data/train.py
@@ -2,35 +2,48 @@ import os
 import pickle
 import click
 import mlflow
+import argparse
 
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error
-
-mlflow.set_tracking_uri("http://127.0.0.1:5000")
 
 def load_pickle(filename: str):
     with open(filename, "rb") as f_in:
         return pickle.load(f_in)
 
-
-@click.command()
-@click.option(
-    "--data_path",
-    default="./output",
-    help="Location where the processed NYC taxi trip data was saved"
-)
 def run_train(data_path: str):
+
+    mlflow.set_tracking_uri("http://127.0.0.1:5000")
+    mlflow.set_experiment("nyc-taxi-trips")
+    mlflow.sklearn.autolog()
 
     X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
     X_val, y_val = load_pickle(os.path.join(data_path, "val.pkl"))
 
-    rf = RandomForestRegressor(max_depth=10, random_state=0)
-    rf.fit(X_train, y_train)
-    y_pred = rf.predict(X_val)
+    with mlflow.start_run():        
+        mlflow.log_param(
+            "train-data-path", os.path.join(data_path, "train.pkl")
+        )
+        mlflow.log_param(
+            "valid-data-path", os.path.join(data_path, "valid.pkl")
+        )
 
-    rmse = mean_squared_error(y_val, y_pred, squared=False)
+        rf = RandomForestRegressor(max_depth=10, random_state=0)
+        rf.fit(X_train, y_train)
+        y_pred = rf.predict(X_val)
+
+        rmse = mean_squared_error(y_val, y_pred, squared=False)
+        mlflow.log_metric("rmse", rmse)
 
 
 if __name__ == '__main__':
-    print(f"tracking URI: '{mlflow.get_tracking_uri()}'")
-    run_train()
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--data_path",
+        default="./output",
+        help="the location where the processed NYC taxi trip data was saved."
+    )
+    args = parser.parse_args()
+
+    run_train(args.data_path)
